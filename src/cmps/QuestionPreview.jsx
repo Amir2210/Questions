2345ac00e0fd96dfe508df1d28d33ebe1c14b051
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-12 13:02:12 +0200

    Create the functionality to show more info about 1 question

--- src/cmps/QuestionPreview.jsx
+++ src/cmps/QuestionPreview.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 export function QuestionPreview({ question }) {
+  const [isShowInfo, setIsShowInfo] = useState(false)
   return (
     <article className='question'>
       <header>
         <h5>{question.title}</h5>
+        <button onClick={() => setIsShowInfo(!isShowInfo)} className='question-btn'>{isShowInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}</button>
       </header>
+      {isShowInfo &&
+        <p>
+          {question.info}
+        </p>}
     </article>
   )
 }
